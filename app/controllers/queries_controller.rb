1f5f550650e9e35ae3677d5e1aa9863c80d099ad
Robert Mosolgo <rdmosolgo@gmail.com>
2015-09-15 21:29:18 -0700

    update for #execute helper

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -5,7 +5,7 @@ class QueriesController < ApplicationController
   def create
     query_string = params[:query]
     query_variables = params[:variables] || {}
-    query = GraphQL::Query.new(StarWarsSchema, query_string, variables: query_variables)
-    render json: query.result
+    result = StarWarsSchema.execute(query_string, variables: query_variables)
+    render json: result
   end
 end
