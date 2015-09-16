57f48ec28ac03d31842e590a6c013c0c6612c759
Irio Musskopf <iirineu@gmail.com>
2015-09-15 23:11:11 -0300

    Iterate over existing constant for defining enum type values
    Though declarative, increases a little bit the complexity/number of
    lines when understanding the repository - thus the library.

--- app/graph/types/episode_enum.rb
+++ app/graph/types/episode_enum.rb
@@ -2,7 +2,7 @@
 EpisodeEnum = GraphQL::EnumType.define do
   name "Episode"
   description "An part of the Star Wars saga"
-  value("NEWHOPE",  "Part 4", value: 4)
-  value("EMPIRE",   "Part 5", value: 5)
-  value("JEDI",     "Part 6", value: 6)
+  Character::EPISODES.each do |episode, name|
+    value(name, "Part #{episode}", value: episode)
+  end
 end
