2e99203cdd446c0300016bc58479dd9b642e35d3
Robert Mosolgo <rdmosolgo@gmail.com>
2016-04-14 14:13:43 -0400

    fix(NodeIdentification) update for new API

--- app/graph/types/node_identification.rb
+++ app/graph/types/node_identification.rb
@@ -2,7 +2,7 @@
 GraphQL::Relay::GlobalNodeIdentification.instance_variable_set(:@instance, nil)
 
 NodeIdentification = GraphQL::Relay::GlobalNodeIdentification.define do
-  object_from_id -> (id) do
+  object_from_id -> (id, ctx) do
     type_name, id = NodeIdentification.from_global_id(id)
     type_name.constantize.find(id)
   end
