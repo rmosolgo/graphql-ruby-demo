a032cfa23e9ce352d3d6a083a1e3aac62af3cfce
Robert Mosolgo <rdmosolgo@gmail.com>
2016-04-15 10:05:01 -0400

    fix(variables) handle query variables as string

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -4,8 +4,18 @@ class QueriesController < ApplicationController
 
   def create
     query_string = params[:query]
-    query_variables = params[:variables] || {}
+    query_variables = ensure_hash(params[:variables] || {})
     result = StarWarsSchema.execute(query_string, variables: query_variables)
     render json: result
+  end
+
+  private
+
+  def ensure_hash(query_variables)
+    if query_variables.is_a?(String)
+      JSON.parse(query_variables)
+    else
+      query_variables
+    end
   end
 end
