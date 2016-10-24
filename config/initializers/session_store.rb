bc8938dd5b18ed8016dabc70cfc8361178448391
Willian van der Velde <mail@willian.io>
2016-10-24 20:26:20 +0200

    Upgrades to Rails 5

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_graphql-ruby-demo_session'
+Rails.application.config.session_store :cookie_store, key: '_graphql_ruby_demo_session'
