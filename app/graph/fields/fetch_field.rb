4f8d4b5e4360f7ad442e4e1d9dcbaab2b75eaa82
willian <willian@hackerone.com>
2017-01-20 11:27:11 -0800

    Remove eager load and add modules

--- app/graph/fields/fetch_field.rb
+++ app/graph/fields/fetch_field.rb
@@ -1,18 +1,20 @@
-class FetchField < GraphQL::Field
-  def initialize(model:, type:)
-    self.type = type
-    @model = model
-    self.description = "Find a #{model.name} by ID"
-    self.arguments = {
-      "id" => GraphQL::Argument.define do
-        name "id"
-        type !GraphQL::INT_TYPE
-        description "Id for record"
-      end
-    }
-  end
+module Fields
+  class FetchField < GraphQL::Field
+    def initialize(model:, type:)
+      self.type = type
+      @model = model
+      self.description = "Find a #{model.name} by ID"
+      self.arguments = {
+        "id" => GraphQL::Argument.define do
+          name "id"
+          type !GraphQL::INT_TYPE
+          description "Id for record"
+        end
+      }
+    end
 
-  def resolve(object, arguments, ctx)
-    @model.find(arguments["id"])
+    def resolve(object, arguments, ctx)
+      @model.find(arguments["id"])
+    end
   end
 end
--- app/graph/types/droid_type.rb
+++ app/graph/types/droid_type.rb
@@ -5,14 +5,16 @@
 #   appearsIn: [Episode]
 #   primaryFunction: String
 # }
-DroidType = GraphQL::ObjectType.define do
-  name "Droid"
-  description "A robotic character in Star Wars"
-  interfaces [GraphQL::Relay::Node.interface, CharacterInterface]
+module Types
+  DroidType = GraphQL::ObjectType.define do
+    name "Droid"
+    description "A robotic character in Star Wars"
+    interfaces [GraphQL::Relay::Node.interface, CharacterInterface]
 
-  global_id_field :id
-  field :name, !types.String, "The name of this droid"
-  connection :friends, CharacterInterface.connection_type, "Friends of this droid"
-  field :appearsIn, types[EpisodeEnum], "Episodes this droid appears in", property: :appears_in
-  field :primaryFunction,  types.String, "What this droid is for", property: :primary_function
+    global_id_field :id
+    field :name, !types.String, "The name of this droid"
+    connection :friends, CharacterInterface.connection_type, "Friends of this droid"
+    field :appearsIn, types[EpisodeEnum], "Episodes this droid appears in", property: :appears_in
+    field :primaryFunction,  types.String, "What this droid is for", property: :primary_function
+  end
 end
